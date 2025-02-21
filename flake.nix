d6d3d8afcbf60a7f10c485ba950590d6ce20a07e
Mattias Kockum <mattias@kockum.net>
2025-02-21 18:11:53 +0100

    Now copying the /etc/nixos/* directory

--- flake.nix
+++ flake.nix
@@ -1,14 +1,28 @@
 {
   description = "Good Practice NixOS";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    #disko.url = "github:nix-community/disko";
+  };
+
+  #outputs = { self, nixpkgs, disko }: {
   outputs = { self, nixpkgs }: {
     GoodPracticeNixosConfigurations = {
       iso = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ({ pkgs, modulesPath, ... }: {
-            imports = [ (modulesPath + "/installer/cd-dvd/installation-cd-minimal.nix") ];
+            imports = [
+              (modulesPath + "/installer/cd-dvd/installation-cd-minimal.nix")
+              #disko.nixosModules.disko
+              #./nixos
+            ];
             isoImage.squashfsCompression = "gzip -Xcompression-level 1";
+
+            system.activationScripts.copy-nixos-files.text = ''
+              mkdir -p /etc/nixos
+              cp -r ${./nixos}/* /etc/nixos/
+            '';
           })
           ./iso.nix
         ];
