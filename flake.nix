2a99870ca8c1c4371145a0965f5709c30e16b6df
Mattias Kockum <mattias@kockum.net>
2025-03-23 00:27:07 +0100

    Working moslty as I want. Still need to resolve that issue with the 'files = builtins.readDir ./.;' being required in the root flake. Maybe going to use a shell script to create a dummy flake containing a path to the config dir and use that flake as an override.

--- flake.nix
+++ flake.nix
@@ -3,13 +3,29 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    disko.url = "github:nix-community/disko";
+    disko.url   = "github:nix-community/disko";
+    # The default configuration directory is the Git-tracked ./nixos.
+    # If you want to override it, the external directory must have a flake.nix.
+    configPath.url = "path:./nixos";
   };
 
-  outputs = { nixpkgs, disko, ... }:
+  outputs = { self, nixpkgs, disko, configPath, ... }:
   let
     system = "x86_64-linux";
+    pkgs   = import nixpkgs { inherit system; };
+
+    # Stage the config directory from the configPath input.
+    # If the input comes from an external flake, it should have a "files" attribute
+    # (or you can adjust this to suit your external flake).
+    stagedConfig =
+      if builtins.pathExists configPath then
+        # We assume the external flake is a directory with a flake.nix.
+        # Here we simply use configPath itself (or you could extract a file set).
+        configPath
+      else
+        ./nixos;
   in {
+    # Define the NixOS configuration.
     nixosConfigurations.autoInstaller = nixpkgs.lib.nixosSystem {
       inherit system;
       modules = [
@@ -21,11 +37,19 @@
           isoImage.squashfsCompression = "gzip -Xcompression-level 1";
           system.activationScripts.copy-nixos-files.text = ''
             mkdir -p /etc/premade-configuration
-            cp -r ${./nixos}/* /etc/premade-configuration/
+            cp -r ${stagedConfig}/* /etc/premade-configuration/
           '';
         })
         ./iso.nix
       ];
     };
+
+    # Build the ISO image.
+    packages.${system}.isoImage =
+      self.nixosConfigurations.autoInstaller.config.system.build.isoImage;
+
+    # Set the default package so that "nix build" picks it up.
+    defaultPackage.${system} = self.packages.${system}.isoImage;
   };
 }
+
