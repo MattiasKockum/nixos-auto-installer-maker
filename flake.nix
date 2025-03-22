14f072588164adffda91a107fb70bcb8c246763f
Mattias Kockum <mattias@kockum.net>
2025-03-22 22:51:13 +0100

    Simpler. For now

--- flake.nix
+++ flake.nix
@@ -6,32 +6,10 @@
     disko.url = "github:nix-community/disko";
   };
 
-  outputs = { self, nixpkgs, disko, ... }@inputs: let
+  outputs = { nixpkgs, disko, ... }:
+  let
     system = "x86_64-linux";
-    pkgs = import nixpkgs {
-      inherit system;
-      overlays = [];
-    };
   in {
-    # The iso build output
-    packages.${system}.isoImage = self.nixosConfigurations.autoInstaller.config.system.build.isoImage;
-
-    # A wrapper script to build + optionally run ISO
-    apps.${system}.default = {
-      type = "app";
-      program = toString (pkgs.writeShellScript "auto-install-cli" ''
-        set -euo pipefail
-
-        # Use nix build to build the ISO
-        nix build .#packages.${system}.isoImage
-
-        echo "ISO built at ./result/iso/"
-      '');
-    };
-
-    defaultApp.${system} = self.apps.${system}.default;
-
-    # Define the NixOS configuration
     nixosConfigurations.autoInstaller = nixpkgs.lib.nixosSystem {
       inherit system;
       modules = [
